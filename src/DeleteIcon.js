f626d8980842f1d5dbc7c37b432f3c71005fcf74
vaj573 <jhonedw573@gmail.com>
2023-11-27 21:08:57 -0500

    Cambios

---
+++ src/DeleteIcon.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TodoIcon } from './TodoIcon';
+
+function DeleteIcon({onDelete}) {
+    return (
+
+
+  <TodoIcon
+        type="delete"
+            color="gray" 
+        onClick={onDelete}    
+    />
+
+
+    )
+
+}
+
+export { DeleteIcon}
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,22 +1,24 @@
+import { CompleteIcon } from './CompleteIcon.js';
+import { DeleteIcon } from './DeleteIcon.js';
+
+
 import './TodoItem.css';
 function TodoItem(props) {
    console.log("Props en TodoItem:", props);
   return (
     <li className='TodoItem'>
-      <span
-        className={`Icon Icon-check ${props.
-          completed ? "Icon-check--active" : ""}`}
-        
-        onClick={props.onComplete} // desde aca hago el llamado a la funcion completeTodo(todo.text) y esta funcion se lleva 
-      >
-        V
-      </span>
+      <CompleteIcon
+        completed={props.completed}
+        onComplete={props.onComplete}
+      
+      />
+     
           <p className={`TodoItem-p ${props.completed ? "TodoItem-p--complete": ""}`} >{props.text}</p>
-      <span className='Icon Icon-delete'
-      onClick={props.onDelete}
-      >
-        X
-      </span>
+      
+      <DeleteIcon onDelete={props.onDelete}/> 
+       
+      
+     
      </li>
 
   );
